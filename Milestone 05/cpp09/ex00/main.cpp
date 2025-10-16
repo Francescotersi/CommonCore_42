d5de87d44a3154e84faeb6e381cffdb36dbc7fa1
Francesco Tersillo <ftersill@c2r1p10.42firenze.it>
2025-10-16 16:19:17 +0200

    fast push

--- Milestone 05/cpp09/ex00/main.cpp
+++ Milestone 05/cpp09/ex00/main.cpp
@@ -2,10 +2,15 @@
 
 int main(int argc, char** argv)
 {
+	if (argc != 2)
+	{
+		std::cout << "Error : wrong number of arguments" << std::endl;
+		return 1;
+	}	
 
 	try
 	{
-	
+		BitcoinExchange bitcoin(argv[1]);
 	}
 	catch (std::exception& e)
 	{
